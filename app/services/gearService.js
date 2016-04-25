b1265c32349f03e818412e63c5603d928a0e907e
Anuj Syal <syal.anuj@gmail>
2016-04-25 21:34:21 +0530

    gear code logic shifted to cloud
    Weather api, get gear data

--- app/services/gearService.js
+++ app/services/gearService.js
@@ -5,32 +5,23 @@ app.factory('GearService', ['$http', '$q', function ($http, $q) {
     var gear = new Gear();
 
     return {
-        getGearList: getGearList,
-        getWeatherData: getWeatherData
+        getWeatherDataFromCloud: getWeatherDataFromCloud
     };
 
-    function getGearList(temperatureGrade, durationGrade, callback) {
-        var query = new Parse.Query(gear);
-        query.equalTo("temperature_grade", temperatureGrade);
-        query.equalTo("duration_grade", durationGrade);
-        query.find({
-            success: function (parseObject) {
-                callback(JSON.parse(JSON.stringify(parseObject)));
-            },
-            error: function (object, error) {
-                // The object was not retrieved successfully.
-                console.log(error);
-            }
-        });
-    };
-
-    function getWeatherData(coordinates, dateTime) {
-        //https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE,TIME--2015-04-13T12:00:00-0400
-        var url = 'https://api.forecast.io/forecast/e11381bd591807eb53abc80fd55e40da/' + coordinates.latitude + ',' + coordinates.longitude + ',' + dateTime;
-        return $http({ method: 'JSONP', url: url, params: {
-            format: 'jsonp',
-            callback: 'JSON_CALLBACK'
+    function getWeatherDataFromCloud(coordinates, dateTime,duration, callback) {
+        var data = {
+            coordinates: coordinates,
+            dateTime: dateTime,
+            duration: duration
         }
+        Parse.Cloud.run("getGearData", data, {
+            success: function (object) {
+                callback(object);
+            },
+
+            error: function (object, error) {
+                console.log(object + ' ' + error);
+            }
         });
     }
 } ]);

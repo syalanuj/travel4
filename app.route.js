d7e839eea6b5588c1d08e419389701792c7264f5
Anuj Syal <syal.anuj@gmail>
2016-05-18 21:14:14 +0530

    chat with campture

--- app.route.js
+++ app.route.js
@@ -23,6 +23,10 @@
         .when('/location/',{
             templateUrl:'app/components/locationComingSoon/locationComingSoon.html'
         })
+        .when('/chatWithCampture/',{
+            controller: 'ChatWithCamptureCtrl',
+            templateUrl:'app/components/chatWithCampture/chatWithCampture.html'
+        })
         .when('/pageNotFound/',{
             controller: 'ErrorPageCtrl',
             templateUrl:'app/components/error/errorPage.html'

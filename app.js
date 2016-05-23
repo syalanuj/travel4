552f3dc3b56e2abc1cc47c85889bf14bca473a3f
sarthak22 <sarthak.chow@gmail.com>
2016-05-23 23:33:56 +0530

    interview form

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
                 Parse.FacebookUtils.init({
 
                     // pro-tip: swap App ID out for PROD App ID automatically on deploy using grunt-replace
-                    appId: 1057485864324708, // Facebook App ID //test1:933421053397857 test2: 1672690479656185 test3: 520223044824428
+                    appId: 520223044824428, // Facebook App ID //test1:933421053397857 test2: 1672690479656185 test3: 520223044824428
                     channelUrl: 'js/facebook/channel.html', // Channel File
                     cookie: true, // enable cookies to allow Parse to access the session
                     xfbml: true, // parse XFBML

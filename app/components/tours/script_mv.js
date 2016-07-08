02d362e7b409504442ccf17e76c37534864266c0
sarthak22 <sarthak.chow@gmail.com>
2016-07-08 17:26:34 +0530

    march trek

--- app/components/tours/script_mv.js
+++ app/components/tours/script_mv.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#sticker").sticky({
-        topSpacing: 40
+        topSpacing: 40,
+        bottomSpacing:570
     });
 });
 (function ($) {
@@ -13,13 +14,19 @@ $(document).ready(function () {
 })(jQuery);
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll >= $(".intro-photo").height() - 40) {
+    var leftDo = $(window).width() - $('.son-width').width()
+    var leftDoTwo = leftDo/2;
+    if (scroll >= $(".intro-photo").height() - 90) {
         $(".sale-info").removeClass("bottom-zero");
         $(".sale-info").addClass("fixed-top");
         $('.sale-info').css('z-index', '3');
+        $(".sale-info").css({'width':($(".green-bodah").width()+ 30 +'px')});
+        $(".sale-info").css({left:(leftDoTwo)+'px'});
     }
     else {
         $(".sale-info").addClass("bottom-zero");
         $(".sale-info").removeClass("fixed-top");
+        $('.sale-info').css('width', '100%');
+        $('.sale-info').css('left', '');
     }
 });
